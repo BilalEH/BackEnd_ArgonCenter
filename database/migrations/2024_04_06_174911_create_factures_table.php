4208577125a5493193c222da7b259025bba68c30
Bilal <sfsdfiuwhef@gmail.com>
2024-04-27 16:28:47 +0100

    v0.9

---
+++ database/migrations/2024_04_06_174911_create_factures_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('factures', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('matere_id')->constrained();
+            $table->foreignId('etudiant_id')->constrained();
+            $table->date('date_paye');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('factures');
+    }
+};
